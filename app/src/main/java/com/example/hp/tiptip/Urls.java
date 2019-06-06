f5e04f5660be793f61655d68841470ae1750a6b0
SpeakingSword <45556848+SpeakingSword@users.noreply.github.com>
2019-06-06 13:02:20 +0800

    Update Urls.java
    some change

--- app/src/main/java/com/example/hp/tiptip/Urls.java
+++ app/src/main/java/com/example/hp/tiptip/Urls.java
@@ -1,7 +1,7 @@
 package com.example.hp.tiptip;
 
 public class Urls {
-    private final static String localhost = "http://10.0.2.2:8080/TipTip/";
+    private final static String localhost = "http://192.168.3.2:8080/TipTip/";
     public final static String loginUrl = localhost+"loginAction";
     public final static String registerUrl = localhost+"registerAction";
     public final static String addFriendUrl = localhost+"addFriendAction";
