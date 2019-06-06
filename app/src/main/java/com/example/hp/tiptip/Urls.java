0e87d134144d0aa4010425ff7b8ff71bad525bbc
SpeakingSword <45556848+SpeakingSword@users.noreply.github.com>
2019-06-06 12:53:14 +0800

    some change

---
+++ app/src/main/java/com/example/hp/tiptip/Urls.java
@@ -0,0 +1,9 @@
+package com.example.hp.tiptip;
+
+public class Urls {
+    private final static String localhost = "http://10.0.2.2:8080/TipTip/";
+    public final static String loginUrl = localhost+"loginAction";
+    public final static String registerUrl = localhost+"registerAction";
+    public final static String addFriendUrl = localhost+"addFriendAction";
+    public final static String getFriendListUrl = localhost+"getFriendListAction";
+}
